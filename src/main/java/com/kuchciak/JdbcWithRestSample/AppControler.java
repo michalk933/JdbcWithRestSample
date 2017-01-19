702c4ae2d0e2a4db99d8ec211c0eeff3667d26e3
michalk933 <6pak.tv@gmail.com>
2017-01-19 23:03:36 +0100

    Method insert new Products

--- src/main/java/com/kuchciak/JdbcWithRestSample/AppControler.java
+++ src/main/java/com/kuchciak/JdbcWithRestSample/AppControler.java
@@ -5,9 +5,9 @@ import info.kuchciak.DataBase.Products;
 import info.kuchciak.DataBase.ProductsRepository;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +18,7 @@ public class AppControler {
 
 
     // Return object Products with productCode = S10_2016
-    @RequestMapping("getEmp")
+    @GetMapping("getEmp")
     public List<Products> EmployList() {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
@@ -28,9 +28,45 @@ public class AppControler {
         List<Products> productses = new ArrayList<>();
         productses.add(sample);
 
+
         return productses;
 
     }
+
+    // Methoad get data from user.
+    // Create new object Products and send to Repository
+    //productCode, productName, productLine, productScale, productVendor, productDescription, quantityInStock, buyPrice, MSRP
+    @PostMapping("addProduct/")
+    public Products addProduct(@RequestParam("productCode") String productCode,
+                               @RequestParam("productName") String productName,
+                               @RequestParam("productLine") String productLine,
+                               @RequestParam("productScale") String productScale,
+                               @RequestParam("productVendor") String productVendor,
+                               @RequestParam("productDescription") String productDescription,
+                               @RequestParam("quantityInStock") int quantityInStock,
+                               @RequestParam("buyPrice") BigDecimal buyPrice,
+                               @RequestParam("MSRP") BigDecimal MSRP) {
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+        ProductsRepository productsRepository = applicationContext.getBean("productsRepository", ProductsRepository.class);
+
+        Products products = new Products();
+        products.setProductCode(productCode);
+        products.setProductName(productName);
+        products.setProductLine(productLine);
+        products.setProductScale(productScale);
+        products.setProductVendor(productVendor);
+        products.setProductDescription(productDescription);
+        products.setQuantityInStock(quantityInStock);
+        products.setBuyPrice(buyPrice);
+        products.setMSRP(MSRP);
+
+        productsRepository.addNewProduct(products);
+
+
+        return products;
+    }
+//'S700_2047', 'HMS Bounty', 'Ships', '1:700', 'Unimax Art Galleries', 'Measures 30 inches Long x 27 1/2 inches High x 4 3/4 inches Wide. \r\nMany extras including rigging, long boats, pilot house, anchors, etc. Comes with three masts, all square-rigged.', '3501', '39.83', '90.52'
 
 
 }
--- src/main/java/info/kuchciak/DataBase/ProductsRepository.java
+++ src/main/java/info/kuchciak/DataBase/ProductsRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
@@ -24,14 +25,14 @@ public class ProductsRepository {
 
     // Methot test
     // Get and show one row
-    public Map<String, Object> getProduct(String productCode){
+    public Map<String, Object> getProduct(String productCode) {
         String sql = "SELECT * FROM products WHERE productCode = ?";
-        return jdbcTemplate.queryForMap(sql,productCode);
+        return jdbcTemplate.queryForMap(sql, productCode);
     }
 
     // Method mapping result query with data base in object Products
     // Get one row with productCode
-    public Products getProductsByProductCode(String productCode){
+    public Products getProductsByProductCode(String productCode) {
         RowMapper<Products> rowMapper = new RowMapper<Products>() {
             @Override
             public Products mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -48,12 +49,23 @@ public class ProductsRepository {
                 return products;
             }
         };
-        return jdbcTemplate.queryForObject("SELECT * FROM products WHERE productCode = ?", rowMapper,productCode);
-
-
+        return jdbcTemplate.queryForObject("SELECT * FROM products WHERE productCode = ?", rowMapper, productCode);
     }
 
 
+    // Method add new record in to Products table 
+    public void addNewProduct(Products products) {
+        System.out.println(products.toString());
+        String sql = "INSERT INTO products " + "(productCode, productName, productLine, productScale, productVendor, productDescription, quantityInStock, buyPrice, MSRP)"
+                + " VALUES (?,?,?,?,?,?,?,?,?)";
+        Object[] objects = new Object[]{products.getProductCode(), products.getProductName(), products.getProductLine(),
+                products.getProductScale(), products.getProductVendor(), products.getProductDescription(),
+                products.getQuantityInStock(), products.getBuyPrice(), products.getMSRP()};
 
+        int update = jdbcTemplate.update(sql, objects);
+        System.out.println("Co zwraca baza : " + update);
 
+    }
+
+//localhost:8080/addProduct/?productCode=S800_8000&productName=Cebula&productLine=Ships&productScale=1:700&productVendor=Unimax&productDescription=CosTamCosTam&quantityInStock=99999&buyPrice=321.65&MSRP=66.66
 }
